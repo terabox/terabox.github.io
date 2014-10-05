7d199c0484bed2d2e48615e5eeae9a1d42f613bf
Pham Hung Tien <tienpham@cloudvis.vn>
2014-10-05 19:09:27 +0700

    update

--- _assets/javascripts/all.js
+++ _assets/javascripts/all.js
@@ -1,1 +1,17 @@
-//= require jquery
+//= require vendor/modernizr
+//= require vendor/jquery
+//= require vendor/bootstrap
+//= require plugins
+//= require views/view.home
+//= require theme
+//= require theme.init
+
+//    <script src="/scripts/plugins.js"></script>
+//    <script src="/scripts/vendor/jquery.easing.js"></script>
+//    <script src="/scripts/vendor/jquery.appear.js"></script>
+//    <script src="/scripts/vendor/jquery.cookie.js"></script>
+//
+//<script src="/scripts/views/view.home.js"></script>
+//<script src="/scripts/theme.js"></script>
+//    <script src="/scripts/theme.init.js"></script>
+//    <script src="/scripts/custom.js"></script>
