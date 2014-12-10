7c6e4df4e2aafa71dcb43503730bff6134d2dc8c
IDarKz <darkz1989@yahoo.com>
2014-12-10 10:36:03 +0700

    update

--- _assets/javascripts/all.js
+++ _assets/javascripts/all.js
@@ -9,13 +9,3 @@
 //= require views/view.home
 //= require theme
 //= require theme.init
-
-//    <script src="/scripts/plugins.js"></script>
-//    <script src="/scripts/vendor/jquery.easing.js"></script>
-//    <script src="/scripts/vendor/jquery.appear.js"></script>
-//    <script src="/scripts/vendor/jquery.cookie.js"></script>
-//
-//<script src="/scripts/views/view.home.js"></script>
-//<script src="/scripts/theme.js"></script>
-//    <script src="/scripts/theme.init.js"></script>
-//    <script src="/scripts/custom.js"></script>
